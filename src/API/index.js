445529e17deb19ee971b081dc88ee707daf8a368
Madhur Jain <mjain.sps@gmail.com>
2021-05-01 19:06:45 +0530

    API done to get data

---
+++ src/API/index.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { API_URL } from "../Constants";
+
+//Function to get Story IDS
+export const getStoryID = async (storyCategory) => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}/${storyCategory}.json/?print=pretty`
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+//Function to get complete stories
+export const getFullStory = async (storyID) => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}/item/${storyID}.json/?print=pretty`
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+//Function to get User Data
+export const getUserData = async (userID) => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}/user/${userID}.json/?print=pretty`
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
