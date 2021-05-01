c5c73f052c47bb329ace4cd12e020b43bd93eb58
Madhur Jain <mjain.sps@gmail.com>
2021-05-01 19:02:36 +0530

    boiler plate ready. Header Component done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header--logo">
+      HACKER
+      <span>NEWS</span>
+    </div>
+  );
+}
+
+export default Header;
